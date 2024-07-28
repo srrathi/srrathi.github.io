120c23de1120988e63bb99bac17e806d83c0930c
Sitaram Rathi <srrathi2000@gmail.com>
2024-07-28 10:43:39 +0530

    added scroll down icon

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -10,49 +10,54 @@ const Hero = () => {
     return (
         <div className='h-full w-full'>
             <div className="overflow-hidden">
-                <div className="grid grid-cols-12 my-auto min-h-screen items-center">
-                    <div className="col-span-12 md:col-span-5 lg:col-span-5">
-                        <OrbAnimation />
-                    </div>
-                    <div className="order-last md:order-first lg:order-first col-span-12 md:col-span-7 lg:col-span-7">
-                        <TagWord text={`Hello, world`} />
-                        <div className='hidden md:block lg:block'>
-                            <div className="text-[40px]/[40px] max-w-[900px]">
-                                <p className="content__container__text">
-                                    I&apos;m Sitaram Rathi a&nbsp;
-                                </p>
-                                <div className="relative">
-                                    <div className="content__container">
-                                        <ul className="content__container__list">
-                                            <li className="content__container__list__item">Software&nbsp;Developer</li>
-                                            <li className="content__container__list__item">Fullstack&nbsp;Developer</li>
-                                            <li className="content__container__list__item">Backend&nbsp;Developer</li>
-                                            <li className="content__container__list__item">Database&nbsp;Developer</li>
-                                        </ul>
+                <div className="min-h-screen">
+                    <div className="grid grid-cols-12 my-auto min-h-[95vh] items-center">
+                        <div className="col-span-12 md:col-span-5 lg:col-span-5">
+                            <OrbAnimation />
+                        </div>
+                        <div className="order-last md:order-first lg:order-first col-span-12 md:col-span-7 lg:col-span-7">
+                            <TagWord text={`Hello, world`} />
+                            <div className='hidden md:block lg:block'>
+                                <div className="text-[40px]/[40px] max-w-[900px]">
+                                    <p className="content__container__text">
+                                        I&apos;m Sitaram Rathi a&nbsp;
+                                    </p>
+                                    <div className="relative">
+                                        <div className="content__container">
+                                            <ul className="content__container__list">
+                                                <li className="content__container__list__item">Software&nbsp;Developer</li>
+                                                <li className="content__container__list__item">Fullstack&nbsp;Developer</li>
+                                                <li className="content__container__list__item">Backend&nbsp;Developer</li>
+                                                <li className="content__container__list__item">Database&nbsp;Developer</li>
+                                            </ul>
+                                        </div>
                                     </div>
                                 </div>
+                                <p className="text-[40px]/[40px] text-left pt-5 m-0">
+                                    from Bengaluru, India.
+                                </p>
                             </div>
-                            <p className="text-[40px]/[40px] text-left pt-5 m-0">
-                                from Bengaluru, India.
-                            </p>
-                        </div>
 
-                        <div className="block md:hidden lg:hidden">
-                            <div className="max-w-[80%]">
-                                <h1 className="text-2xl font-semibold text-white">
-                                    <span>I&apos;m Sitaram Rathi a</span>
-                                    <TextAnimation texts={[
-                                        <p key={1}>Software&nbsp;Developer</p>,
-                                        <p key={2}>Fullstack&nbsp;Developer</p>,
-                                        <p key={3}>Backend&nbsp;Developer</p>,
-                                        <p key={4}>Database&nbsp;Developer</p>,
-                                        <p key={5}>Web&nbsp;Developer</p>,
-                                    ]} interval={3000} />
-                                    <p className='pt-2'>from Bengaluru, India.</p>
-                                </h1>
+                            <div className="block md:hidden lg:hidden">
+                                <div className="max-w-[80%]">
+                                    <h1 className="text-2xl font-semibold text-white">
+                                        <span>I&apos;m Sitaram Rathi a</span>
+                                        <TextAnimation texts={[
+                                            <p key={1}>Software&nbsp;Developer</p>,
+                                            <p key={2}>Fullstack&nbsp;Developer</p>,
+                                            <p key={3}>Backend&nbsp;Developer</p>,
+                                            <p key={4}>Database&nbsp;Developer</p>,
+                                            <p key={5}>Web&nbsp;Developer</p>,
+                                        ]} interval={3000} />
+                                        <p className='pt-2'>from Bengaluru, India.</p>
+                                    </h1>
+                                </div>
                             </div>
+                            <ButtonElement href='#contact' text='CONTACT ME' />
                         </div>
-                        <ButtonElement href='#contact' text='CONTACT ME' />
+                    </div>
+                    <div className="flex w-full justify-center">
+                        <div className="mouse"></div>
                     </div>
                 </div>
 
